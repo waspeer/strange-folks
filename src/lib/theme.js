790708dabe9cab69a7b792f5a6d74a61cdfd7427
Wannes Salomé <mail@wannessalome.nl>
2020-01-17 09:34:04 +0100

    fixed safari bug where svg didnt work

--- src/lib/theme.js
+++ src/lib/theme.js
@@ -1,9 +1,12 @@
-import textBackground from "../images/klang.svg"
+import textBackgroundDay from "../images/klang-day.svg"
+import textBackgroundNight from "../images/klang-night.svg"
 
 export default ({ mode }) => ({
   backgroundColor: mode === "light" ? "#fff2d3" : "#559cd6",
   primary: "#ff88d9",
   secondary: "#00feff",
   neutral: "#ffe000",
-  textBackgroundUrl: `${textBackground}#${mode === "light" ? "day" : "night"}`,
+  textBackgroundUrl: `${
+    mode === "light" ? textBackgroundDay : textBackgroundNight
+  }`,
 })
